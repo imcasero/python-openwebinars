2e4118e169895045964e77dff274f39ee8d89bd1
imcasero <diegocaserosmr@gmail.com>
2023-01-29 19:15:19 +0100

    init

---
+++ cap_1/index.py
@@ -0,0 +1,9 @@
+numero = 5;
+if numero == 5:
+    print ("Hola mundo dentro") #para separar bloques tienes que tabular
+print ("Hola mundo fuera")
+
+if numero == 5: print ("Hola mundo dentro") #tambien se puede escribir en una linea
+
+if numero == 5 and 1 == 1 and \
+2==2 : print ("Hola") #con la barra se puede escribir en una linea difernet
